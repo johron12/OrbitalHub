e236fe5445ac15ecea20fd7cf9d47be02664a604
JohanDev <johanclipss@gmail.com>
2022-01-17 16:18:09 +0100

    Update loader.lua

---
+++ src/loader.lua
@@ -0,0 +1,22 @@
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/lib/main/main.lua'))()
+local win = lib:Window("ORBITAL", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+local tab = win:Tab("Loader")
+
+
+local passcode = ""
+
+
+tab:Label("Passcode")
+
+tab:Textbox("Passcode", true, function(t)
+    passcode = t
+end)
+
+tab:Button("Submit", function()
+    if t == "orbital" then
+        lib:Notification("Orbital Loader", "Hello!", "Correct passcode")
+    else
+        lib:Notification("Orbital Loader", "Hello!", "Wrong passcode!")
+    end
+    end
+end)
