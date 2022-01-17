ca29280fb1941c1f75a218d46f21b35733305ced
JohanDev <johanclipss@gmail.com>
2022-01-17 19:33:01 +0100

    2

--- src/games/default.lua
+++ src/games/default.lua
@@ -1,4 +1,4 @@
-function default:init(lib, win, tab)
+local function init(lib, win, tab)
     local plr = win:Tab("Player")
     local world = win:Tab("World")
     local visuals = win:Tab("Visuals")
