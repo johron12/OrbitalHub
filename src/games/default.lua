609d44316962d02ad8ac20d8df659395849afadd
JohanDev <johanclipss@gmail.com>
2022-01-17 19:36:16 +0100

    123

--- src/games/default.lua
+++
@@ -1,7 +0,0 @@
-local function init(lib, win, tab)
-    local plr = win:Tab("Player")
-    local world = win:Tab("World")
-    local visuals = win:Tab("Visuals")
-
-    plr:Label("Comming Soon.")
-end
--- src/main.lua
+++ src/main.lua
@@ -2,7 +2,7 @@ local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/JohanDevv
 local win = lib:Window("ORBITAL", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
 local tab = win:Tab("Welcome")
 
---local default = loadstring(game:HttpGet('https://raw.githubusercontent.com/JohanDevv/OrbitalHub/main/src/games/default.lua'))()
+local loader = loadstring(game:HttpGet('https://raw.githubusercontent.com/JohanDevv/OrbitalHub/main/src/loader.lua'))()
 
 tab:Label("Welcome to OrbitalHub!")
 tab:Label("Please enter a passcode to load the hub.")
@@ -10,7 +10,7 @@ tab:Label("Please enter a passcode to load the hub.")
 tab:Textbox("Passcode", true, function(t)
 	if t == "alpha" then
 		lib:Notification("Orbital Loader", "Correct Credentials", "Load")
-        default.init(lib, win, tab)
+
 	else
 		lib:Notification("Orbital Loader", "Incorrect Credentials", "Try Again.")
 	end
