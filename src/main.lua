f275f693f541139d4ed4070ddd100199d38ced9a
JohanDev <johanclipss@gmail.com>
2022-01-18 14:15:37 +0100

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -17,7 +17,7 @@ local function loadGame()
     elseif game.PlaceId == games.Arsenal then
         loadstring(game:HttpGet('https://raw.githubusercontent.com/JohanDevv/OrbitalHub/main/src/games/arsenal.lua'))()
     else
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/JohanDevv/OrbitalHub/main/src/games/test.lua'))().load(lib, win)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/JohanDevv/OrbitalHub/main/src/games/test.lua'))()
     end
 end
 
